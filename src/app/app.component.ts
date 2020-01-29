f264fbf84f19bd4c5db15d953546c485030d67f2
Aravindan <aravindannichu007@gmail.com>
2020-01-29 22:33:30 +0530

    fileupload sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {HttpClientModule, HttpClient, HttpRequest, HttpResponse, HttpEventType} from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fileUpload';
+  constructor(private http: HttpClient){
+    
+  }
+  upload(files: File[]){
+    //pick from one of the 4 styles of file uploads below
+    this.uploadAndProgress(files);
+  }
+
+  uploadAndProgress(files: File[]){
+    console.log(files)
+    var formData = new FormData();
+    Array.from(files).forEach(f => formData.append('file[]',f))
+    
+    this.http.post('http://localhost:3000', formData)
+      .subscribe(res => {
+        console.log('res',res)
+    });
+  }
 }
